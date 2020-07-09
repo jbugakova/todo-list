995a9891efcb0a5c9769416549ddbdf2d505d055
jbugakova <bugakovayk@gmail.com>
2020-07-09 14:59:21 +0300

    Merged dropdown-logic.js with app.js, implemented add, delete, sort tasks

---
+++ src/js/api.js
@@ -0,0 +1,28 @@
+const TODOS_URL = 'https://5eecb0734cbc3400163308c9.mockapi.io/todos';
+
+export default {
+    getTodos,
+    addTodo,
+    deleteTodo
+};
+
+function getTodos() {
+    return fetch(TODOS_URL)
+            .then(response => response.json());
+}
+
+function addTodo(todo) {
+    return fetch(TODOS_URL, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(todo)
+            }).then(response => response.json());
+}
+
+function deleteTodo(id) {
+    return fetch(TODOS_URL + '/' + id, {
+                method: 'DELETE'
+            }).then(response => response.json());
+}
--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,165 @@
 import '../scss/styles.scss';
 
-import './dropdown-logic.js';
+import api from './api';
+
+const dropdownBtn = document.querySelector('.panel__dropdown-btn');
+const dropdownList = document.querySelector('.panel__dropdown-list');
+const dropdownOverlay = document.querySelector('.panel__dropdown-overlay');
+const addTaskForm = document.querySelector('#addTaskForm');
+const todoListItemTemplate = document.querySelector('#todoListItemTemplate').innerHTML;
+const todoList = document.querySelector('#todoList');
+const todoInput = document.querySelector('.add-task-form__input');
+
+let todos = [];
+
+init();
+
+document.querySelector('.panel_propdown-conatiner').addEventListener('click', onDropdownClick);
+addTaskForm.addEventListener('submit', onAddTaskFormSubmit);
+todoList.addEventListener('click', onListClick);
+
+function onDropdownClick(e) {
+    var target = e.target;
+    switch(true) {
+        case(target.classList.contains('panel__dropdown-btn') || target.classList.contains('arrow') || target.classList.contains('value')):
+            onDropdownBtnClick();
+            break;
+        case(target.classList.contains('panel__dropdown-list-item')):
+            ondropdownListClick(target.innerText, target.dataset.value);
+            break;
+        case(target.classList.contains('panel__dropdown-overlay')):
+            onDropdownOverlayClick();
+            break;
+    }
+}
+
+function onListClick(e) {
+    const target = e.target;
+    
+    if(target.closest('.list__btn-delete')) {
+        const id = target.closest('.list__item').dataset.id;
+        deleteTodo(id);
+    } else if(target.closest('.list__btn-done')) {
+        const id = target.closest('.list__item').dataset.id;
+    }
+}
+
+function onAddTaskFormSubmit(e) {
+    e.preventDefault();
+    const input = getInputValue();
+
+    if(input) {
+        const todo = {
+            title: input
+        };
+
+        api.addTodo(todo)
+            .then(addTodoToArray)
+            .then(renderTodo);
+
+        addTaskForm.reset();
+    }
+}
+
+function deleteTodo(id) {
+    api.deleteTodo(id)
+        .then(deleteTodoFromArray);
+
+    deleteElemFromPage(id);
+}
+
+function deleteTodoFromArray(elem) {
+    todos = todos.filter(todo => todo.id != elem.id);
+}
+
+function deleteElemFromPage(id) {
+    const el = getDOMElementByDataId(id);
+    el.remove();
+}
+
+function getDOMElementByDataId(id) {
+    return document.querySelector(`.list__item[data-id='${id}']`);
+}
+
+function getInputValue() {
+    return todoInput.value.trim();
+}
+
+function addTodoToArray(todo) {
+    todos.push(todo);
+
+    return todo;
+}
+
+function init() {
+    getTodos();
+}
+
+function getTodos() {
+    api.getTodos()
+        .then(setTodos)
+        .then(renderTodos);
+}
+
+function setTodos(data) {
+    return (todos = data);
+}
+
+function renderTodos(todos) {
+    todos.map(renderTodo);
+}
+
+function renderTodo(todo) {
+    const html = todoListItemTemplate.replace('{{id}}', todo.id)
+                                        .replace('{{todo}}', todo.title);
+
+    const todoElement = htmlToElement(html);
+    if(todo.completed) {
+        todoElement.classList.add('list__item-done');
+    }
+
+    todoList.append(todoElement);
+}
+
+function htmlToElement(html) {
+    const template = document.createElement('template');
+    template.innerHTML = html.trim();
+
+    return template.content.firstChild;
+}
+
+function onDropdownBtnClick() {
+    dropdownBtn.classList.toggle('open');
+    dropdownList.classList.toggle('active');
+    dropdownOverlay.classList.toggle('active');
+}
+
+function ondropdownListClick(text, value) {
+    switchBtnText(text);
+    closeDropDown();
+    showSortedList(value);
+}
+
+function showSortedList(value) {
+    todoList.innerHTML = '';
+    if(value === 'all') {
+        renderTodos(todos);
+    } else if(value === 'completed') {
+        renderTodos(todos.filter(item => item.completed === true));
+    } else if(value === 'incompleted') {
+        renderTodos(todos.filter(item => item.completed === false));
+    }
+}
+
+function switchBtnText(text) {
+    document.querySelector('.panel__dropdown-btn .value').innerText = text;
+}
+
+function onDropdownOverlayClick() {
+    closeDropDown();
+}
+
+function closeDropDown() {
+    dropdownList.classList.remove('active');
+    dropdownOverlay.classList.remove('active');
+}
